df54a1dd3c0733653a699f1fcb1721a5405c5729
cyandream-gerrit <kontakt@yanniks.de>
2013-10-17 21:49:32 -0400

    initial CyanDream commit

--- cd.mk
+++ cd.mk
@@ -2,16 +2,16 @@ TARGET_KERNEL_SOURCE := kernel/goldfish
 TARGET_KERNEL_CONFIG := cyanogenmod_goldfish_armv7_defconfig
 
 # Inherit CM common GSM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/cyandream/config/gsm.mk)
 
 # Inherit CM common Phone stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cyandream/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/generic/goldfish/goldfish.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_goldfish
+PRODUCT_NAME := cd_goldfish
 PRODUCT_DEVICE := generic
 PRODUCT_BRAND := CyanogenMod
 PRODUCT_MODEL := Full CyanogenMod on Emulator
